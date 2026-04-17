90032519b90e94c04d134408f10b1b6a8645a8d0
alex <apadmos@github.com>
2026-04-17 13:45:51 -0400

    working on different length handling for DbColumn

--- db_table_definition.py
+++ db_table_definition.py
@@ -1,44 +1,4 @@
-class DbColumnDefinition(object):
-    RESERVED = {"user", "object"}
-
-    def __init__(self, name: str,
-                 data_type: str,
-                 nullable: bool = False,
-                 is_primary_key: bool = False,
-                 is_unique: bool = False,
-                 default=None
-                 ):
-        self.name = name
-        if name in DbColumnDefinition.RESERVED:
-            self.name = f"\"{name}\""
-        self.data_type = data_type.upper()
-        self.data_type = self.data_type
-        self.nullable = nullable
-        self.is_primary_key = is_primary_key
-        self.is_unique = is_unique
-        self.default = default
-
-    def copy(self):
-        return DbColumnDefinition(name=self.name,
-                                  data_type=self.data_type,
-                                  nullable=self.nullable,
-                                  is_primary_key=self.is_primary_key,
-                                  is_unique=self.is_unique,
-                                  default=self.default)
-
-    def __str__(self):
-        null = "NULL" if self.nullable else "NOT NULL"
-        default = f"default {self.default}" if self.default else ""
-        return f"{self.name} {self.data_type} {default} {null}"
-
-    def __eq__(self, other):
-        return str(self).__eq__(str(other))
-
-    def __le__(self, other):
-        return str(self).__le__(str(other))
-
-    def __gt__(self, other):
-        return str(self).__gt__(str(other))
+from .db_column_definition import DbColumnDefinition
 
 
 class DbTableDefinition(object):
